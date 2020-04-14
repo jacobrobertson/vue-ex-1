bf76471807cc8fd0a58ff045e01455191f51932c
Jacob Robertson <jacob.cs.robertson@gmail.com>
2020-04-14 17:28:35 -0500

    faces

---
+++ src/components/Faces.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-container>
+    <v-row v-for="r in facerows" :key="r">
+      <v-col v-for="c in facecols" :key="c">
+        <v-img 
+			:src="randomface(faceicons)" 
+			:width="facesize"></v-img>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Faces",
+  props: {
+		facerows: Number, 
+		facecols: Number,
+		facesize: Number,
+  },
+  data: () => ({
+	testicon: "../assets/faces/boy-smiling.png",
+    faceicons: [
+      "amazed-man.png",
+      "angry-man.png",
+      "angry-woman.png",
+      "baby-crying.png",
+      "baby-love.png",
+      "boy-broad-smile.png",
+      "boy-happy-smile.png",
+      "boy-smiling.png",
+      "boy-suffering.png",
+      "delighted-granny.png",
+      "disappointed-boy.png",
+      "embarrased-boy.png",
+      "embarrased-girl.png",
+      "embarrased-granny.png",
+      "emotive-granny.png",
+      "fat-boy-angry.png",
+      "fat-boy-shocked.png",
+      "fat-boy-smiling.png",
+      "fat-boy-sorry.png",
+      "fat-boy.png",
+      "frightened-hipster.png",
+      "girl-air-kissing.png",
+      "girl-crying.png",
+      "girl-embarrased.png",
+      "girl-laughing-to-tears.png",
+      "girl-showing-tongue.png",
+      "girl-smiling.png",
+      "girl-with-poker-face.png",
+      "happy-baby.png",
+      "happy-woman.png",
+      "hipster-smiling.png",
+      "hypnotized-hipster.png",
+      "inexpressive-girl.png",
+      "kissing-girl.png",
+      "man-with-moustache-smiling.png",
+      "naughty-girl.png",
+      "perplexed-man.png",
+      "philosophizing-boy.png",
+      "sad-baby.png",
+      "sad-girl.png",
+      "sad-hipster.png",
+      "sad-woman.png",
+      "satisfied-woman.png",
+      "shocked-girl.png",
+      "sleeping-granny.png",
+      "sleepy-boy.png",
+      "smiling-baby.png",
+      "smiling-girl.png",
+      "surprised-girl.png",
+      "suspicious-man.png",
+      "teasing-boy.png",
+      "upset-girl.png",
+      "upset-granny.png",
+      "winking-boy.png",
+      "wondered-hipster.png"
+	],
+	randomface: function(faceicons) {
+		var index = Math.floor(Math.random() * (faceicons.length - 1));
+		return require("../assets/faces/" + faceicons[index]); 
+	},
+  })
+};
+</script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,28 +14,29 @@
         </v-select>
       </v-col>
     </v-row>
+
+    <v-container v-if="selectedsummoner.name">
     <v-row>
       <v-col>
         <v-card class="mx-auto">
           <v-toolbar>
-            <v-toolbar-title class="grey--text">{{ selectedsummoner.name }} played against X players like these in the last 10 games...</v-toolbar-title>
+            <v-toolbar-title class="grey--text">{{ selectedsummoner.name }} played against X players including these in their last 10 games...</v-toolbar-title>
             <v-spacer></v-spacer>
 
-            <v-tooltip v-model="show" top max-width="50em">
+            <v-tooltip top max-width="50em">
               <template v-slot:activator="{ on }">
                 <v-btn icon v-on="on">
                   <v-icon color="grey lighten-1">mdi-information</v-icon>
                 </v-btn>
               </template>
-              <span>{{ selectedsummoner.name }} was matched against X players like these in the last X games, 
+              <span>{{ selectedsummoner.name }} was matched against X players including these in the last X games, 
                 which means they are the types of players that {{ selectedsummoner.name }}
                 will be matched against in upcoming games.</span>
             </v-tooltip>
-
           </v-toolbar>
-          <v-divider></v-divider>
 
-          <v-card-text style="height: 200px;">
+          <Faces id="abc2" :facerows="1" :facecols="8" facesize="60" />
+          <v-card-text style="height: 150px;">
             <vue-word-cloud
                 :words="yoursummoners"
                 :color="(value) => wordcloudcolor(value)"
@@ -52,15 +53,22 @@
       <v-col>
         <v-card class="mx-auto">
           <v-toolbar>
-            <v-toolbar-title class="grey--text">...and those X players played against X other players like these in their last X games.</v-toolbar-title>
+            <v-toolbar-title class="grey--text">...and those X players played against X other players including these in their last X games.</v-toolbar-title>
             <v-spacer></v-spacer>
-            <v-btn icon>
-              <v-icon>mdi-information</v-icon>
-            </v-btn>
+            <v-tooltip top max-width="50em">
+              <template v-slot:activator="{ on }">
+                <v-btn icon v-on="on">
+                  <v-icon color="grey lighten-1">mdi-information</v-icon>
+                </v-btn>
+              </template>
+              <span>{{ selectedsummoner.name }} might not have played against these players, but they're the types
+                of players they're likely to be matched up against in upcoming games.
+              </span>
+            </v-tooltip>
           </v-toolbar>
-          <v-divider></v-divider>
 
-          <v-card-text style="height: 200px;">
+          <Faces id="abc1" :facerows="2" :facecols="16" facesize="25" />
+          <v-card-text style="height: 135px;">
             <vue-word-cloud
                 :words="theirsummoners"
                 :color="(value) => wordcloudcolor(value)"
@@ -92,14 +100,17 @@
         </v-card>
       </v-col>
     </v-row>
-
+    </v-container>
   </v-container>
 </template>
 
 <script>
+  import Faces from './Faces.vue'
   export default {
     name: 'HelloWorld',
-
+    components: {
+      Faces
+    },
     data: () => ({
       title: 'hello kitty',
       selectedsummoner: {},
@@ -123,8 +134,7 @@
         return (12 - value.length) * 2;
       },
       yoursummoners: [
-        'abcdefg', 'akdj33', 'zzzzabcdefg',
-        'isse et scelerisque',
+        /*
 'tellus Integer cons',
 'equat dict',
 'um massa',
@@ -153,13 +163,49 @@
 'mpor ipsum a ma',
 'lesuada',
 'accumsan felis nibh',
+'corper s',
+'it amet justo Se',
+'d ultrices tempo',
+'r odio a posuere',
+'Etiam ac posuere q',
+'uam vitae ullamcor',
+'per ligula',
+'Phasellus ut m',
+'assa lacinia sce',
+'lerisque mas',
+'sa vitae',
+'aliquam',
+'risus Duis blandit',
+'purus sit',
+'amet te',
+'llus volutpat l',
+'acinia Praesent sce',
+'lerisque p',
+'urus vitae',
+'lorem finib',
+'us non mattis purus',
+'venenatis Aliq',
+'uam ac ex',
+'sit amet',
+'enim cursus fringill',
+'a Sed sed a',
+'nte pellen',
+'tesque auctor felis',
+'non elementum',
+'ante Etia',
+'m tempus nunc magna',
+'eget pel',
+*/
+'lentesque',
+'enim aliquam ut C',
+'ras et ege',
+'stas arcu',
+'in aliquam veli',
+'t Donec fini',
+'bus velit port',
+'titor fringilla pla',
       ],
       theirsummoners: [
-        'sydfsdf-abcdefger', 'akdjf2433', 'zzzzabcdefg',
-        'ysdfsdfcdefg', '4akdjfs32433 sd fd', 'z4zzdefg',
-        'sdfysdf-abcdefg', '3akdjfs32433', 'zzabcdefg',
-        'sdfsy-abcdefg', '1akdj433', 'zzzabdefg',
-        'Lorem ips',
 'um dolor sit',
 'amet consec',
 'tetur adi',
@@ -194,6 +240,7 @@
 'blandit',
 'tellus Prae',
 'sent et orci et null',
+/*
 'a iaculis tristi',
 'que Vestibulum in s',
 'emper velit i',
@@ -218,8 +265,121 @@
 'Vivamus v',
 'ulputate',
 'non urna sit ame',
-
-
+'vitae commodo lectus',
+'Maecenas vel',
+'nunc nunc',
+'Nullam sapie',
+'n felis vestibul',
+'um sed co',
+'nvallis a cur',
+'sus quis m',
+'assa Sed in',
+'velit d',
+'ui Morbi',
+'gravida lobortis maxi',
+'mus Nullam condiment',
+'um enim sit amet vel',
+'it ultricies',
+'non lacinia nunc v',
+'ehicula Viv',
+'amus bibend',
+'um nec quam id eg',
+'estas Suspend',
+'isse et scelerisque',
+'tellus Integer cons',
+'equat dict',
+'um massa',
+'eget plac',
+'erat erat Etiam',
+'posuere in felis soda',
+'les mollis',
+'Proin blandit',
+'erat sed ex pla',
+'cerat rhoncu',
+'s Nullam a elit',
+'ut odio sod',
+'ales lobor',
+'tis at vel d',
+'uiIn rutru',
+'m pharetra vi',
+'verra Sed da',
+'pibus ris',
+'us eget eros viverra',
+'interdum Donec pr',
+'etium ipsum p',
+'orta dapibus justo',
+'non pos',
+'uere neque Donec',
+'ut lorem',
+'sit amet tortor s',
+'celerisque varius ac',
+'ut metus Dui',
+'s maximu',
+'s nulla at nibh phare',
+'tra molestie',
+'Cras hend',
+'rerit lor',
+'em orci quis eleifen',
+'d sapien',
+'ultricies in Sus',
+'pendisse viverra n',
+'isi in imperdiet sus',
+'cipit Curab',
+'itur tempus dolo',
+'r vel ph',
+'aretra pretium lacu',
+'s lacus consequat v',
+'elit et pretium ni',
+'si felis at s',
+'apien Fusce',
+'et bibendum le',
+'ctus Nul',
+'la consequat',
+'scelerisque alique',
+'t Mauris',
+'non felis sit',
+'amet sapien grav',
+'ida pulvinarMauri',
+'s rutrum nun',
+'c quis risus finibus',
+'tempor Cras',
+'lobortis',
+'id libero eg',
+'et lacinia Vivamus p',
+'ulvinar ex nib',
+'h fringil',
+'la maximus a',
+'ugue dapibus quis',
+'Maecenas pulvina',
+'r mauris quis',
+'maximus sodales',
+'felis lorem digniss',
+'im augue',
+'in sollicit',
+'udin eros',
+'lorem in n',
+'isl Vivam',
+'us condiment',
+'um augue libero nec',
+'faucibus lacus tem',
+'por a P',
+'ellentes',
+'que pharetra diam id',
+'ipsum rhoncus ac te',
+'mpor massa',
+'pellentesque Quisq',
+'ue dictum eu nisi e',
+'get euismod Proin a',
+'liquet sod',
+'ales lobortis Qu',
+'isque vel nun',
+'c vehicula a',
+'liquam ipsum vel',
+'imperdiet sem Ut',
+'lacinia',
+'orci vel temp',
+'or tincidunt risu',
+*/
       ],
       headers: [
         {
